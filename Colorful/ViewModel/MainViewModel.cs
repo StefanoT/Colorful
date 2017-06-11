06646aefe619bd46427d30d6e30da2a1067ecac7
Stefano Tommesani <stefano@tommesani.com>
2017-06-11 20:23:29 -0300

    uses service for open file dialog

--- Colorful/ViewModel/MainViewModel.cs
+++ Colorful/ViewModel/MainViewModel.cs
@@ -1,8 +1,32 @@
-﻿using GalaSoft.MvvmLight;
+﻿/*
+Project: Colorful
+Author: Stefano Tommesani
+Website: http://www.tommesani.com
+Microsoft Public License (MS-PL) [OSI Approved License]
+This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+1. Definitions
+The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+A "contribution" is the original software, or any additions or changes to the software.
+A "contributor" is any person that distributes its contribution under this license.
+"Licensed patents" are a contributor's patent claims that read directly on its contribution.
+2. Grant of Rights
+(A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+(B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+3. Conditions and Limitations
+(A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+(B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+(C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+(D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+(E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+*/
+
+using GalaSoft.MvvmLight;
 using Colorful.Model;
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Threading.Tasks;
+using MvvmDialogs;
+using MvvmDialogs.FrameworkDialogs.OpenFile;
 
 namespace Colorful.ViewModel
 {
@@ -57,20 +81,25 @@ namespace Colorful.ViewModel
             private set;
         }
 
+        private readonly IDialogService _dialogService;
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
-        public MainViewModel(IColorService colorService)
+        public MainViewModel(IColorService colorService, IDialogService dialogService)
         {
             _colorService = colorService;
+            _dialogService = dialogService;
 
             PickImageCommand = new RelayCommand(
                 PickImage);
         }
 
+        /// <summary>
+        /// run measure computation in a separate task, then continue with the UI update with results
+        /// </summary>
         private void ComputeColorfulness()
-        {
-            // run measure computation in a separate task, then continue with the UI update with results
+        {            
             Task computeTask = Task.Factory.StartNew(() => _colorService.ComputeColorIndex(ImageFileName)).ContinueWith((t) =>
             {
                 ColorIndex = t.Result;
@@ -82,18 +111,18 @@ namespace Colorful.ViewModel
         /// </summary>
         private void PickImage()
         {
-            // create the OpenFileDialog object
-            Microsoft.Win32.OpenFileDialog openPicker = new Microsoft.Win32.OpenFileDialog();
-            // Add file filters           
-            openPicker.DefaultExt = ".jpg";
-            openPicker.Filter = "Image Files|*.jpg;*.jpeg";
-            // display the OpenFileDialog by calling ShowDialog method
-            Nullable<bool> result = openPicker.ShowDialog();
-            // check to see if we have a result 
-            if (result == true)
+            var settings = new OpenFileDialogSettings
+            {
+                Title = "Pick an image",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Filter = "Jpeg images (*.jpg)|*.jpg;*.jpeg|All Files (*.*)|*.*"
+            };
+
+            bool? success = _dialogService.ShowOpenFileDialog(this, settings);
+            if (success == true)
             {
                 ColorIndex = null;
-                ImageFileName = openPicker.FileName.ToString();
+                ImageFileName = settings.FileName;                               
                 ComputeColorfulness();
             }
         }
