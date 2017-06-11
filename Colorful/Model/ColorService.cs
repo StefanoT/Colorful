01454969486f8bc1bb26134a0dc26dc0d75e0bee
Stefano Tommesani <stefano@tommesani.com>
2017-06-11 18:37:38 -0300

    first commit

---
+++ Colorful/Model/ColorService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+
+namespace Colorful.Model
+{
+    public class ColorService : IColorService
+    {
+        /// <summary>
+        /// implements image colorfulness measure with the Hasler and Süsstrunk algorithm
+        /// </summary>
+        /// <param name="imageFileName">input image file to process</param>
+        /// <returns>colorfulness measure</returns>
+        /// <remarks>http://www.pyimagesearch.com/2017/06/05/computing-image-colorfulness-with-opencv-and-python/</remarks>
+        public double ComputeColorIndex(string imageFileName)
+        {
+            Bitmap sourceImage;
+            try
+            {
+                sourceImage = new Bitmap(imageFileName);
+            } catch (Exception e)
+            {
+                sourceImage = null;
+            }
+            if (sourceImage == null)
+                return 0;
+
+            int totalPixels = sourceImage.Width * sourceImage.Height;
+            if (totalPixels <= 0)
+                return 0;
+            // compute mean
+            double rg_mean = 0;
+            double yb_mean = 0;
+            for (int y = 0; y < sourceImage.Height; y++)
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    Color currentPixel = sourceImage.GetPixel(x, y);
+                    rg_mean += Math.Abs((int)currentPixel.R - (int)currentPixel.G);
+                    yb_mean += Math.Abs(0.5 * ((int)currentPixel.R + (int)currentPixel.G) - currentPixel.B);
+                }
+            rg_mean /= totalPixels;
+            yb_mean /= totalPixels;
+            double mean = Math.Sqrt(Math.Pow(rg_mean, 2) + Math.Pow(yb_mean, 2));
+            // compute standard deviation
+            double rg_stddev = 0;
+            double yb_stddev = 0;
+            for (int y = 0; y < sourceImage.Height; y++)
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    Color currentPixel = sourceImage.GetPixel(x, y);
+                    double rg = Math.Abs((int)currentPixel.R - (int)currentPixel.G);
+                    double yb = Math.Abs(0.5 * ((int)currentPixel.R + (int)currentPixel.G) - currentPixel.B);
+                    rg_stddev += Math.Pow(rg - rg_mean, 2);
+                    yb_stddev += Math.Pow(yb - yb_mean, 2);
+                }
+            rg_stddev /= totalPixels;
+            rg_stddev = Math.Sqrt(rg_stddev);
+            yb_stddev /= totalPixels;
+            yb_stddev = Math.Sqrt(yb_stddev);
+            double stddev = Math.Sqrt(Math.Pow(rg_stddev, 2) + Math.Pow(yb_stddev, 2));
+
+            return 0.3 * mean + stddev;
+        }
+    }
+}
---
+++ Colorful/ViewModel/MainViewModel.cs
@@ -0,0 +1,101 @@
+﻿using GalaSoft.MvvmLight;
+using Colorful.Model;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Threading.Tasks;
+
+namespace Colorful.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IColorService _colorService;
+
+        private string _imageFileName = string.Empty;
+        private double? _colorIndex = null;
+
+        /// <summary>
+        /// Gets the imageFileName property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string ImageFileName
+        {
+            get
+            {
+                return _imageFileName;
+            }
+            set
+            {
+                Set(ref _imageFileName, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets the imageFileName property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public double? ColorIndex
+        {
+            get
+            {
+                return _colorIndex;
+            }
+            set
+            {
+                Set(ref _colorIndex, value);
+            }
+        }
+
+        public RelayCommand PickImageCommand
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IColorService colorService)
+        {
+            _colorService = colorService;
+
+            PickImageCommand = new RelayCommand(
+                PickImage);
+        }
+
+        private void ComputeColorfulness()
+        {
+            // run measure computation in a separate task, then continue with the UI update with results
+            Task computeTask = Task.Factory.StartNew(() => _colorService.ComputeColorIndex(ImageFileName)).ContinueWith((t) =>
+            {
+                ColorIndex = t.Result;
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        /// <summary>
+        /// let the user select an image and then compute the colorfulness measure
+        /// </summary>
+        private void PickImage()
+        {
+            // create the OpenFileDialog object
+            Microsoft.Win32.OpenFileDialog openPicker = new Microsoft.Win32.OpenFileDialog();
+            // Add file filters           
+            openPicker.DefaultExt = ".jpg";
+            openPicker.Filter = "Image Files|*.jpg;*.jpeg";
+            // display the OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openPicker.ShowDialog();
+            // check to see if we have a result 
+            if (result == true)
+            {
+                ColorIndex = null;
+                ImageFileName = openPicker.FileName.ToString();
+                ComputeColorfulness();
+            }
+        }
+    }
+}
